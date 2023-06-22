ff9e789a7e884ddad7cf536b7786bb42885fd2c6
QRen1 <renchest.quiray.f@bulsu.edu.ph>
2023-06-22 15:54:01 +0800

    waaaaaaaaaa

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import leavesone from '../assets/bg-decals.svg'
 import './Body.css'
 import appstore from '../assets/aapp.webp'
 import playstore from '../assets/pp.png'
+import disc from '../assets/product-search.svg'
 
 const Body = () => {
     return (
@@ -26,6 +27,21 @@ const Body = () => {
             <div class="stores">
                 <img src={playstore} class="play"></img>
                 <img src={appstore} class="app"></img>
+            </div>
+
+            <div class="meet">
+                <div class="protoapp">
+                    <header>Meet Protoapp</header>
+                    <h2>The app lorem ipsum is common placeholder text used to demonstrate the graphic elements of a document or visual representation.</h2>
+                </div>
+                <div class="discover">
+                    <div class="divT">
+                        <h1>Discover</h1>
+                        <h3>Get information on using these guidelines whenever you are using therefore they have the same style and quality</h3>
+                    </div>
+                    <img src={disc}></img>
+                </div>
+                <div class="aug"></div>
             </div>
         </div>
         
